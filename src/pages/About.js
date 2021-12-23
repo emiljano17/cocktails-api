17707a4c8e622e620e1016ecf914407d0e3612d7
Emiljano <emiljanodemirxhiu15@gmail.com>
2021-12-23 13:34:41 +0100

    react cocktails api

---
+++ src/pages/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Velit cupidatat sunt adipisicing proident. Cillum deserunt dolore sit
+        amet. Sint deserunt labore aliqua aliquip voluptate eu sunt commodo ad
+        commodo nostrud tempor. Sit non cillum esse proident ullamco ea
+        consequat tempor ea ullamco occaecat ad. Voluptate nisi aute officia
+        magna ullamco velit cillum nisi consectetur. Mollit excepteur qui
+        occaecat Lorem mollit consectetur do proident laboris. Ad aute tempor
+        laborum minim ut veniam sunt anim Lorem irure qui. Tempor commodo ut
+        excepteur laboris sunt exercitation qui anim. Esse esse sunt quis id
+        exercitation anim minim est consectetur ut commodo irure cupidatat.
+        Deserunt cillum mollit incididunt adipisicing duis quis commodo
+        excepteur velit excepteur et. Veniam magna Lorem dolor do ullamco eu. In
+        ea aliquip in adipisicing esse id officia Lorem nulla adipisicing
+        deserunt nulla. Lorem veniam eiusmod in do nisi Lorem elit ad dolore
+        incididunt eu incididunt aliqua cupidatat. Minim adipisicing qui anim
+        velit in aliqua laborum velit non velit.
+      </p>
+    </section>
+  );
+};
+
+export default About;
